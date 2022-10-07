c9ef6b910d76dcbfcca3788476e057bba76d9c59
Glory Menga <glory.menga@student.ehb.be>
2022-10-07 18:46:03 +0200

    spaceinvader done

--- spaceInvader.js
+++ spaceInvader.js
@@ -13,17 +13,17 @@ function drawspaceinvader(){
 
 
     context.lineWidth = 1;
-    context.strokeStyle = 'white';
+    context.strokeStyle = '#F0F0F0';
     context.beginPath();
-    context.fillStyle = 'ghostwhite';
+    context.fillStyle = '#F0F0F0';
     context.rect(50,50,300,300);
     context.fill();
     context.stroke();
 
     context.lineWidth = 1;
-    context.strokeStyle = 'cornflowerblue';
+    context.strokeStyle = '#8A9ED9';
     context.beginPath();
-    context.fillStyle =  'cornflowerblue';
+    context.fillStyle =  '#8A9ED9';
     context.rect(75,75,50,50);
     context.rect(175,75,50,250);
     context.rect(125,175,150,100);
